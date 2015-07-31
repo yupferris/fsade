df8a16131ff9020fd80b133cf5c7c2bbdbeae3d3
ferris <jake@fusetools.com>
2015-07-31 15:04:04 -0700

    some refactoring and filling in data

--- src/Project.hs
+++ src/Project.hs
@@ -12,11 +12,29 @@ data Project = Project { projectName :: String
 createDefaultProject name =
   Project { projectName = name }
 
-serializeProject project =
-  ppTopElement $ Element { elName = unqual "Project"
-                         , elAttribs = []
-                         , elContent = []
-                         , elLine = Nothing
-                         }
+serializeProject = ppTopElement . serializeProjectXml
+
+serializeProjectXml project =
+  unode "Project"
+    ([ attr "ToolsVersion" "12.0"
+     , attr "DefaultTargets" "Build"
+     , attr "xmlns" "http://schemas.microsoft.com/developer/msbuild/2003"
+     ],
+     [ unode "Import"
+       [ attr "Project" "$(MSBuildExtensionsPath)\\$(MSBuildToolsVersion)\\Microsoft.Common.props"
+       , attr "Condition" "Exists('$(MSBuildExtensionsPath)\\$(MSBuildToolsVersion)\\Microsoft.Common.props')"
+       ]
+     , unode "PropertyGroup"
+       [ unode "Configuration"
+         (attr "Condition" " '$(Configuration)' == '' ",
+          "Debug")
+       , unode "Platform"
+         (attr "Condition" " '$(Platform)' == '' ",
+          "AnyCPU")
+       , unode "SchemaVersion" "2.0"
+       ]
+     ])
+
+attr name value = Attr (unqual name) value
 
 serializeProjectFile filePath = writeFile filePath . serializeProject
